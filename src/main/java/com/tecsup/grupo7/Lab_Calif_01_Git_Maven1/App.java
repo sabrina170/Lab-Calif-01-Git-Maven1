9dbbb254021d4b21cb07107db2532d2bc8e12a1a
sabrina170 <sabrina.pomajulca@tecsup.edu.pe>
2019-03-21 13:25:35 -0500

    Primer Commit

---
+++ src/main/java/com/tecsup/grupo7/Lab_Calif_01_Git_Maven1/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.grupo7.Lab_Calif_01_Git_Maven1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
