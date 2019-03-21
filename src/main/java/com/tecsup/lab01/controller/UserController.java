fcdddb1af28874316631a8a408716d5592ac4b0e
sabrina170 <sabrina.pomajulca@tecsup.edu.pe>
2019-03-21 14:28:39 -0500

    comit control2

--- src/main/java/com/tecsup/lab01/controller/UserController.java
+++ src/main/java/com/tecsup/lab01/controller/UserController.java
@@ -5,7 +5,7 @@ import com.tecsup.lab01.model.UserModel;
 public class UserController extends UserModel{
 	
    public String obtenernombre() {
-	   return nombre + " "+ apellidos + " " + edad;
+	   return nombre + " "+ Apellido+ " " + edad;
    }
 	
 }
